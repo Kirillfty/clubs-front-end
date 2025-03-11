ab4be87b512c29ad76e7fb794b47ce0c1ddb24b3
Kirillfty <kirillhaiduk4@gmail.com>
2025-03-11 21:04:05 +0300

    create a user page with user data

--- front/vue-project/src/usersPage.vue
+++ front/vue-project/src/usersPage.vue
@@ -1,8 +1,20 @@
 <template>
-    <div class="cont">
-        <input type="text" v-model="title" >
-        <input type="text" v-model="description">
-        <button @click="Post()">click</button>
+<header>
+<div class="user-header">
+    <div class="logo"></div>
+    <div class="user-text">
+        <p>{{userData.firstName}}</p>
+        <p>{{userData.lastName}}</p>
+        <p>{{userData.nickName}}</p>
+    </div>
+</div>
+<div class="cont">
+    <p>Создать клуб</p>
+    <div class="cont-reg">
+        <input type="text" v-model="title" class="input">
+        <input type="text" v-model="description" class="input">
+        <button @click="Post()" class="sign">click</button>
+    </div>
         <p>clubs</p>
         <div v-for="club in clubs" :key="club" class="clubs">
         <div class="club">
@@ -10,36 +22,71 @@
           <div>
             <p>{{club.title}}</p>
             <p>{{club.description}}</p>
+            <button @click="Delete(club.id)" class="sign"> Delete</button>
           </div>
         </div>
       </div>
 </div>
+</header>
 </template>
+<style>
+    .logo{
+        width:10vh;
+        height:10vh;
+        background-color:yellow;
+        border-radius:5px;
+    }
+    .user-header{
+        width:100%;
+        height:10vh;
+        background-color: grey;
+        display: flex;
+        justify-content: space-around;
+    }
+    .user-text{
+        width:100%;
+        color: aliceblue;
+        font-size: 1em;
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+    }
+</style>
 <script setup> 
 import axios from 'axios'
 import {ref,onMounted} from 'vue'
 import { useRouter} from 'vue-router'
 let router = useRouter();
 let clubs = ref('');
-async function fetchData()
-{
+let title = ref('');
+let description = ref('')
+let result = ref('');
+let userData = ref('');
 
+async function getClubs()
+{
    await axios.get('https://localhost:7210/clubs')
      .then(function (res) {
        console.log(res);
        return clubs.value = res.data;
 })
-
 }
-let title = ref('');
-let description = ref('')
-let result = ref('');
-
-
+async function Delete(id){
+    axios.delete(`https://localhost:7210/clubs/${id}`).then(function(res){
+        if(res){
+            getClubs();
+        }
+    })
+    console.log(id.value);
+    console.log(id);
+}
 async function getUser(){
     console.log("result "+result)
     await axios.get('https://localhost:7210/api/users/'+result.value).then(function(responce){
         console.log(responce.data);
+         userData.value = responce.data;
+         console.log('user '+userData);
+         return userData;
     })
 
 }
@@ -47,10 +94,12 @@ function Post(){
     axios.post('https://localhost:7210/clubs/create',{
         title:title.value,
         description:description.value
-    })
-
+    }).then(function(responce){
+        getClubs();
+    })   
 }
-async function Page(){
+
+async function checkAuthorize(){
     let acsecc = localStorage.getItem('accessToken');
     let refresh = localStorage.getItem('refreshToken');
     if(acsecc != null && refresh != null){
@@ -65,8 +114,8 @@ async function Page(){
     }
 }
 onMounted(async()=>{
-    await fetchData();
-    await Page();
+    await getClubs();
+    await checkAuthorize();
     await getUser();
 })
 </script>
